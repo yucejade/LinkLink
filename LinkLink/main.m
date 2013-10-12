1968a35a4ab664b2290643ae4f4155f1b66d15da
yucejade <yubin@yubinmatoMac-mini.local>
2013-10-12 18:16:21 +0800

    Initial Commit

---
+++ LinkLink/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkLink
+//
+//  Created by yuce on 13-10-12.
+//  Copyright yuce 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
